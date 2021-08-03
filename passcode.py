70f16b613779d383d9faf14e5dfe54677d786e22
Binamin-hussein100 <binamin.hassan@student.moringaschool.com>
2021-08-03 05:21:02 +0300

    added a save function

--- passcode.py
+++ passcode.py
@@ -1,12 +1,32 @@
+import pyperclip
+from credencials_all import Credencials
+
 class Details:
     """
-    generates new instances of details  
+    generates new instances of account 
     """
     
     storage_list = []
-    def __init__(self,Username,password):
+    def __init__(self,Username,password,user):
         self.name = Username
         self.passcode = password
+        self.user = user
         
     def save(self):
+        """
+        saves users to the storage list
+        """
         Details.storage_list.append(self)
+    
+    # @classmethod
+    # def display_accounts(cls,user):
+    #     for account in cls.credencials_list:
+    #         if account.user == user:
+    #             cls.user_accounts
+    
+    @classmethod
+    def log_in(cls,profile_name,sec):
+        for user in storage_list:
+            if user.profile_name == profile_name and user.password == sec:
+                return user 
+    
